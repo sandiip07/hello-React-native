b827c66be502d9e37c995b0535af4593f0531103
sandiip07 <sandiip2021@gmail.com>
2022-07-22 03:42:54 -0700

    hello

--- App.js
+++ App.js
@@ -1,52 +1,20 @@
-import React, { useState } from 'react';
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button } from 'react-native';
+// import React, { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-    
-    let [counter , setCounter] = useState(0);
-
-    Increment = () => {
-        setCounter(counter++);
-        console.log(counter);
-    }
-
-    Decrement = () => {
-        setCounter(counter--);
-    }
-
-    Reset = () => {
-        setCounter(counter*0);
-    }
-
-    return(
-        <View>
-
-            <Text style={{textAline:'center'}}> counter: </Text>
-            <Text>{counter}</Text>
-            <Button
-             title='Increament' 
-             onPress={Increment}
-             />
-
-            <Button 
-            title='Decreament' 
-            onPress={Decrement}
-            />
-
-            <Button 
-            title='Reset'
-             onPress={Reset}
-             />
-
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!!!</Text>
+      <StatusBar style="auto"/>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
